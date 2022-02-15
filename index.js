354d76dab59282a08cb636657f66256c8ac50322
krishnailkar <86242893+krishnailkar@users.noreply.github.com>
2022-02-15 15:23:17 +0530

    complete Project

---
+++ index.js
@@ -0,0 +1,103 @@
+const form = document.getElementById('form');
+const username = document.getElementById('username');
+const email = document.getElementById('email');
+const password = document.getElementById('password');
+const password2 = document.getElementById('password2');
+
+form.addEventListener('submit', e => {
+	e.preventDefault();
+	
+	checkInputs();
+});
+
+function checkInputs() {
+	// trim to remove the whitespaces
+	const usernameValue = username.value.trim();
+	const emailValue = email.value.trim();
+	const passwordValue = password.value.trim();
+	const password2Value = password2.value.trim();
+	
+	if(usernameValue === '') {
+		setErrorFor(username, 'Username cannot be blank');
+	} else {
+		setSuccessFor(username);
+	}
+	
+	if(emailValue === '') {
+		setErrorFor(email, 'Email cannot be blank');
+	} else if (!isEmail(emailValue)) {
+		setErrorFor(email, 'Not a valid email');
+	} else {
+		setSuccessFor(email);
+	}
+	
+	if(passwordValue === '') {
+		setErrorFor(password, 'Password cannot be blank');
+	} else if (passwordValue.length < 8) {
+		setErrorFor(password, 'Password should have minimum 8 character');
+	}
+    else if (passwordValue.search(/[0-9]/)==-1) {
+		setErrorFor(password, 'Password should have atleast 1 numeric character');
+	}
+    else if (passwordValue.search(/[a-z]/)==-1) {
+		setErrorFor(password, 'Password should have atleast 1 Lowercase character');
+	}
+    else if (passwordValue.search(/[A-Z]/)==-1) {
+		setErrorFor(password, 'Password should have atleast 1 Uppercase character');
+	}else if (passwordValue.search(/[!\@\#\$\%\^\&\*\(\)\-\_\+\-\=\<\>\,\.\?]/)==-1) {
+		setErrorFor(password, 'Password should have atleast 1 Special character');
+	} else {
+		setSuccessFor(password);
+	}
+
+	if(password2Value === '') {
+		setErrorFor(password2, 'Password2 cannot be blank');
+	} else if(passwordValue !== password2Value) {
+		setErrorFor(password2, 'Passwords does not match');
+	} else if (password2Value.length < 8) {
+		setErrorFor(password2, 'Password should have minimum 8 character');
+	} else {
+		setSuccessFor(password2);
+	}
+}
+
+function setErrorFor(input, message) {
+	const formControl = input.parentElement;
+	const small = formControl.querySelector('small');
+	formControl.className = 'form-control error';
+	small.innerText = message;
+}
+
+function setSuccessFor(input) {
+	const formControl = input.parentElement;
+	formControl.className = 'form-control success';
+}
+	
+function isEmail(email) {
+	return /^(([^<>()\[\]\\.,;:\s@"]+(\.[^<>()\[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/.test(email);
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+// SOCIAL PANEL JS
+const floating_btn = document.querySelector('.floating-btn');
+const close_btn = document.querySelector('.close-btn');
+const social_panel_container = document.querySelector('.social-panel-container');
+
+floating_btn.addEventListener('click', () => {
+	social_panel_container.classList.toggle('visible')
+});
+
+close_btn.addEventListener('click', () => {
+	social_panel_container.classList.remove('visible')
+});
